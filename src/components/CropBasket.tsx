b3ef234e1851a8ba5352658c9bc6bae9bf76a289
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2024-05-28 00:37:47 +0700

    feat: create CropBasket component

---
+++ src/components/CropBasket.tsx
@@ -0,0 +1,27 @@
+import { type Crop, type CropType } from "@/lib/mock";
+import CustomButton from "./common/buttons/CustomButton";
+
+type Props = {
+  label: CropType;
+  data: Crop[];
+  onClick: (crop: Crop) => void;
+};
+
+export default function CropBasket({ label, data, onClick }: Props) {
+  return (
+    <div className="col-span-4 border border-slate-200 dark:border-slate-800 overflow-hidden rounded-lg">
+      <div className="bg-slate-50 dark:text-white dark:bg-slate-800 border-b border-b-slate-200 dark:border-b-slate-800 font-medium text-center py-2">
+        {label}
+      </div>
+      <div className="flex flex-col gap-3 p-4">
+        {data.map((crop, i) => (
+          <CustomButton
+            key={`CustomButton-${label}-${i}`}
+            label={crop.name}
+            onClick={() => onClick(crop)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
